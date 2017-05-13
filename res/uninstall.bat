2e91b3712f61c2094b251cd82b4da727760f83f2
pepo-le <hello@pepo-le.com>
2017-05-13 21:58:46 +0900

    [modify] v2.0.2 uninstall.batを修正

--- res/uninstall.bat
+++ res/uninstall.bat
@@ -1,9 +1,15 @@
 @echo off
-
-if EXIST %LOCALAPPDATA%\TweetDeckNW (
-    rmdir /S /Q %LOCALAPPDATA%\TweetDeckNW
+Setlocal
+set /P pushKey="Do you want to uninstall(y/n)? : %pushKey%"
+if "%pushKey%"=="y" (
+    if EXIST %LOCALAPPDATA%\TweetDeckNW (
+        rmdir /S /Q %LOCALAPPDATA%\TweetDeckNW
+        echo Uninstall completed.
+    ) else (
+        echo Profile not found. Uninstall canceled.
+    )
+) else (
+    echo Uninstall canceled.
 )
-
-echo Uninstall completed.
 
 pause > nul
